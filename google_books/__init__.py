ff9fd6cde5e183b246d86837730bb47b9576d930
klinga <klingaroo@gmail.com>
2024-01-02 23:46:21 -0500

    add oclc function

--- google_books/__init__.py
+++ google_books/__init__.py
@@ -3,6 +3,7 @@ from collections.abc import Callable
 import click
 
 from google_books.hathi_report import parse_report as hathi_report
+from google_books.marc_manipulator import manipulate_records as fix_oclc_data
 
 
 @click.group()
@@ -20,9 +21,19 @@ def io_params(f: Callable) -> Callable:
 def hathi(source_fh: str) -> None:
     """
     Run analysis of the HathiTrust/Zephir reports and create actionable data.
-    Outputs reports to out/ directory.
+    Outputs reports to files/out/ directory.
     """
     hathi_report(source_fh)
+
+
+@cli.command()
+@io_params
+def oclc(source_fh: str) -> None:
+    """
+    Replaces 001/003 with OCLC info found in 035 or 991. Deletes 991.
+    Outputs manipulated file to files/out/ directory
+    """
+    fix_oclc_data(source_fh)
 
 
 def main() -> None:
