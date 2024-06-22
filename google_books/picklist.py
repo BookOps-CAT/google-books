00a70707d22d84fdf140e9906cb9261aa8b3fb87
klinga <klingaroo@gmail.com>
2024-06-22 09:34:14 -0400

    fix prep_item_list_for_sierra & add date to sierra to dataframe method

--- google_books/picklist.py
+++ google_books/picklist.py
@@ -33,30 +33,38 @@ def prep_item_list_for_sierra(tar_file: str, list_size: int) -> None:
         list_size (int): The number of items to include in the list.
     """
     date = fh_date(tar_file)
-    out = f"files/picklist/nypl-{date}-candidate-items.csv"
 
     extract_candidate_list(tar_file)
 
     # read each extracted .txt file, find item #, and write it to a new file
     files = glob.glob("files/picklist/*_combined-*.txt")
-    n = 0
-    f = 0
+    n = -1
+    s = 1
     for f in files:
         reader = csv.reader(open(f, "r", encoding="utf-8"), delimiter="\t")
         for row in reader:
-            n = +1
-            if n > list_size:
+            n += 1
+            if n >= list_size:
                 n = 0
-                f += 1
-                print(f"Outputting to file -{str(f).zfill(1)}...")
+                s += 1
+                print(f"Outputting to file: {str(s).zfill(3)}...")
             item = row[1][1:]
-            out = f"files/picklist/nypl-{date}-candidate-items-{str(f).zfill(1)}.csv"
+            out = f"files/picklist/nypl-{date}-candidate-items-{str(s).zfill(3)}.csv"
             save2csv(out, ",", [item])
 
 
-def prep_sierra_export_for_dataframe(fh):
+def prep_sierra_export_for_dataframe(fh: str, date: str) -> None:
+    """
+    Transforms a given Sierra export file to a format that can be used to create
+    `pandas.DataFrame` object. Will append data to the out file if already exists.
+
+    Args:
+        fh (str): The path to the Sierra export file
+        date (str): The date of the export in the fomrmat YYYY-MM-DD
+    """
     with open(fh, "r", encoding="utf-8", errors="replace") as f:
         reader = csv.reader(f, delimiter="\t")
+        next(reader)  # skip the header
         for row in reader:
             try:
                 assert len(row[14:]) % 7 == 0
@@ -65,8 +73,8 @@ def prep_sierra_export_for_dataframe(fh):
             linked_bibs_no = int(len(row[14:]) / 7)
             clean_bib_codes = [i for i in islice(row[14:], 0, None, linked_bibs_no)]
             new_row = row[:14] + clean_bib_codes
-            save2csv("files/picklist/google-candidates-clean.txt", "\t", new_row)
-
-
-if __name__ == "__main__":
-    prep_sierra_export_for_dataframe("files/picklist/google-candidates.txt")
+            save2csv(
+                f"files/picklist/candidate-sierra-export-clean-{date}.txt",
+                "\t",
+                new_row,
+            )
