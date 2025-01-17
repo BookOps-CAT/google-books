019c81a5b85789f01d8ccfbec6fbede6f216b388
klinga <klingaroo@gmail.com>
2025-01-17 13:30:28 -0500

    fixes test_new_shipment

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from click.testing import CliRunner
 from google_books import cli
 
@@ -5,6 +7,13 @@ from google_books import cli
 def test_new_shipment(tmp_path):
     runner = CliRunner()
     with runner.isolated_filesystem(temp_dir=tmp_path) as td:
-        result = runner.invoke(cli, ["new-shipment", "19991231", td])
+        cwd = Path(td)
+        f = cwd / "files"
+        f.mkdir()
+        s = f / "shipments"
+        s.mkdir()
+
+        result = runner.invoke(cli, ["new-shipment", "19991231"])
         assert result.exit_code == 0
-        assert result.output == f"New shipment directory created at {td}\\1999-12-31\n"
+        output = Path("files/shipments/1999-12-31")
+        assert result.output == f"New shipment directory created at {output}\n"
